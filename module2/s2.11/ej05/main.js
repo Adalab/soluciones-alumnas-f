4cdedc32300297ac298ea8b19bab7a9181dc572f
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-30 12:34:39 +0200

    add null option

--- module2/s2.11/ej05/main.js
+++ module2/s2.11/ej05/main.js
@@ -7,19 +7,21 @@ const input2 = document.querySelector('#color_dark');
 
 let savedColor = localStorage.getItem('color');
 
-    if (savedColor === 'light') {
-        input1.setAttribute('checked', '');
-        body.classList.remove('dark');
-        body.classList.add('light');
-    } else if (savedColor === 'dark') {
-        input2.setAttribute('checked', '');
-        body.classList.remove('light');
-        body.classList.add('dark');
-    } else {
-        input1.setAttribute('checked', '');
-        body.classList.remove('dark');
-        body.classList.add('light');
-    }
+if (savedColor === null) {
+    input1.setAttribute('checked', '');
+    body.classList.remove('dark');
+    body.classList.add('light');
+    
+} else if (savedColor === 'light') {
+    input1.setAttribute('checked', '');
+    body.classList.remove('dark');
+    body.classList.add('light');
+        
+} else {
+    input2.setAttribute('checked', '');
+    body.classList.remove('light');
+    body.classList.add('dark');    
+} 
 
 function changeColor(event) {
     const guilty = event.currentTarget.value;
