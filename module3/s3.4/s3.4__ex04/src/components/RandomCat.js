82a7b607bd052b976d446313c48d777f22de662a
Megan Edwards <megmyfedwards@gmail.com>
2019-05-07 19:33:35 +0200

    Update RandomCat with logical operator

--- module3/s3.4/s3.4__ex04/src/components/RandomCat.js
+++ module3/s3.4/s3.4__ex04/src/components/RandomCat.js
@@ -6,16 +6,8 @@ const NUMBER_OF_CATS = 10;
 class RandomCat extends React.Component {
   render() {
     const randomCat = getRandomInteger(NUMBER_OF_CATS);
-    let width;
-    let height;
-
-    if (!Object.keys(this.props).length) {
-      width = 400;
-      height = 200;
-    } else {
-      width = this.props.width;
-      height = this.props.height;
-    };
+    let width = this.props.width || 400;
+    let height = this.props.height || 200;
     return (
       <a href="http://lorempixel.com">
         <img src={`http://lorempixel.com/${width}/${height}/cats/${randomCat}`} alt="Random cat"/>
