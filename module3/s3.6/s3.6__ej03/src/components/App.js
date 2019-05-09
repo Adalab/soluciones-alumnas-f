b8b9a183bb80d88a87e0c671c81180c1e4500830
Megan Edwards <megmyfedwards@gmail.com>
2019-05-09 16:15:21 +0200

    Add solution to module 3.6, exercise 3

---
+++ module3/s3.6/s3.6__ej03/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import OnionHater from './OnionHater';
+
+class App extends React.Component {
+  render() {
+    
+    return (
+      <OnionHater />
+    );
+  }
+}
+
+export default App;
