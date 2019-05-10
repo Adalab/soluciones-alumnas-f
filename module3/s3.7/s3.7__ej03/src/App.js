afc7c58fd2797bd81be04eb94abc2517d25f95c9
anniavdc <anva1189@gmail.com>
2019-05-10 19:08:12 +0200

    Add solution

---
+++ module3/s3.7/s3.7__ej03/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date()
+    };
+    this.myTimer = this.myTimer.bind(this);
+    setInterval(this.myTimer, 1000);
+  }
+
+  myTimer() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    return <div>{this.state.time.toLocaleTimeString()}</div>;
+  }
+}
+
+export default Clock;
