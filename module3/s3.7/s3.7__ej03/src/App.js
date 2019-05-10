156d2d56db4ad29ca6ae6b785f45a0b65e53c7ff
anniavdc <anva1189@gmail.com>
2019-05-10 21:01:34 +0200

    Add props

--- module3/s3.7/s3.7__ej03/src/App.js
+++ module3/s3.7/s3.7__ej03/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 class Clock extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       time: new Date()
     };
